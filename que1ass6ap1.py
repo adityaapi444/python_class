4316c0f934294c467944dbee9126f3c27abd77d3
Aditya Apishte <adityaapi444@gmail.com>
2018-09-02 23:23:07 +0530

    assignment 6

---
+++ que1ass6ap1.py
@@ -0,0 +1,5 @@
+#approch 1
+sub_list=['maths','phy','chem','java','mech']
+marks=[78,74,68,77,67]
+d={k:marks.pop(0) for k in sub_list}
+print(d)
---
+++ que1ass6ap2.py
@@ -0,0 +1,4 @@
+sub_list=['maths','phy','chem','java','mech']
+marks=[78,74,68,77,67]
+d=dict(zip(sub_list,marks))
+print(d)
